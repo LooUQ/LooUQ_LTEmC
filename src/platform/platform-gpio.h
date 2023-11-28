e7c1ba5914aa40e36515e63b94f492bfc9718826
Greg Terrell <greg@loouq.com>
2023-11-28 17:06:19 -0500

    remove dependency on Arduino.h from gpio.h

--- src/platform/platform-gpio.h
+++ src/platform/platform-gpio.h
@@ -42,9 +42,35 @@ Also add information on how to contact you by electronic and paper mail.
 #include <stdint.h>
 #include <stdbool.h>
 
-//#define GPIO_NM 9
+#define LOW                 0x0
+#define HIGH                0x1
 
-#ifdef ARDUINO
+//GPIO FUNCTIONS
+#define INPUT               0x01
+// Changed OUTPUT from 0x02 to behave the same as Arduino pinMode(pin,OUTPUT) 
+// where you can read the state of pin even when it is set as OUTPUT
+#define OUTPUT              0x03 
+#define PULLUP              0x04
+#define INPUT_PULLUP        0x05
+#define PULLDOWN            0x08
+#define INPUT_PULLDOWN      0x09
+#define OPEN_DRAIN          0x10
+#define OUTPUT_OPEN_DRAIN   0x13
+#define ANALOG              0xC0
+
+//Interrupt Modes
+#define DISABLED            0x00
+#define RISING              0x01
+#define FALLING             0x02
+#define CHANGE              0x03
+#define ONLOW               0x04
+#define ONHIGH              0x05
+#define ONLOW_WE            0x0C
+#define ONHIGH_WE           0x0D
+
+
+// #ifdef ARDUINO
+// #include <Arduino.h>
 
 typedef enum {
     gpioValue_low = LOW,
@@ -66,7 +92,7 @@ typedef enum {
     gpioIrqTriggerOn_rising = RISING
 } gpioIrqTrigger_t;
 
-#endif 
+//#endif 
 
 // typedef uint8_t platformGpioPin;
 typedef void(*platformGpioPinIrqCallback)(void);
